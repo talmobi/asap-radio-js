50d5ca8fcc2bc4ad0e9a257248c37b8b8220a88b
Grant Galitz <grantgalitz@gmail.com>
2011-05-22 22:29:14 -0400

    Revert

--- XAudioServer.js
+++ XAudioServer.js
@@ -107,13 +107,7 @@ XAudioServer.prototype.executeCallback = function () {
 		//mozAudio:
 		var samplesRequested = webAudioMinBufferSize - this.remainingBuffer();
 		if (samplesRequested > 0) {
-			var samplesOutputted = this.underRunCallback(samplesRequested);
-			var samplesAccepted = this.audioHandle.mozWriteAudio(samplesOutputted);
-			this.samplesAlreadyWritten += samplesAccepted;
-			if (samplesOutputted.length > samplesAccepted) {
-				//Make the buffering lower if we outputted too many samples:
-				webAudioMinBufferSize -= samplesOutputted.length - samplesAccepted;
-			}
+			this.samplesAlreadyWritten += this.audioHandle.mozWriteAudio(this.underRunCallback(samplesRequested));
 		}
 	}
 	else if (this.audioType == 1) {
